bba43c17d5ea4478d085874bb1dc95e74ea15499
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2021-09-23 21:25:38 +0300

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -36,13 +36,13 @@ uploaded_file = st.file_uploader("Choose an image...", type=['tif'])
 
 if uploaded_file is not None:
     
-    #image = Image.open(uploaded_file)
+    t_img = Image.open(uploaded_file)
     tfile = tempfile.NamedTemporaryFile(delete=False)
     tfile.write(uploaded_file.read())
     image = cv2.imread(tfile.name,-1)
     #t_img = Image.fromarray(image/2**3)
     
-    #st.image(t_img, caption='Uploaded Image.', use_column_width=False)
+    st.image(t_img, caption='Uploaded Image.', use_column_width=False)
 
     button = st.button("Let's Predict Image")
 
@@ -50,7 +50,7 @@ if uploaded_file is not None:
         t = st.empty()
         #t.markdown('## İmage is segmenting...')
         t.markdown(f'{image.shape}')
-        """ model,session = loading_model()
+        model,session = loading_model()
         K.set_session(session)
         image = np.array(image,dtype='uint16').reshape((1,512,512,3))
         result_img = model.predict(image)
@@ -58,4 +58,4 @@ if uploaded_file is not None:
         result_img = result_img[0,:,:,0]
         result_img = Image.fromarray(result_img)
         t.markdown('## Segmentation result: ')
-        st.image(result_img, caption='Predicted Image.', use_column_width=False) """
+        st.image(result_img, caption='Predicted Image.', use_column_width=False)
